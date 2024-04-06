526bfc477ef919631074065ac0c020bddc048321
Lukas Rastinger <lukas.rastinger@gmail.com>
2024-04-06 20:15:16 +0200

    Changes implementation.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,10 +14,12 @@ func main() {
 	byteCountPtr := flag.Bool("c", false, "Counts the bytes of a file.")
 	lineCountPtr := flag.Bool("l", false, "Counts the lines of a file.")
 	wordCountPtr := flag.Bool("w", false, "Counts the words of a file.")
+	charCountPtr := flag.Bool("m", false, "Counts the characters of a file.")
 
 	flag.Parse()
 
 	filePath := flag.Args()[0]
+	areFlagsSet := flag.NFlag() > 0
 
 	var input io.Reader
 
@@ -35,38 +38,42 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *byteCountPtr {
-		byteCount := countBySplit(input, bufio.ScanBytes)
-		fmt.Println(fmt.Sprintf("%v\t%v", byteCount, filePath))
-		return
+	lineCount, wordCount, charCount, byteCount := count(input)
+
+	if *lineCountPtr || !areFlagsSet {
+		fmt.Printf("%v\t", lineCount)
 	}
 
-	if *lineCountPtr {
-		lineCount := countBySplit(input, bufio.ScanLines)
-		fmt.Println(fmt.Sprintf("%v\t%v", lineCount, filePath))
-		return
+	if *wordCountPtr || !areFlagsSet {
+		fmt.Printf("%v\t", wordCount)
 	}
 
-	if *wordCountPtr {
-		wordCount := countBySplit(input, bufio.ScanWords)
-		fmt.Println(fmt.Sprintf("%v\t%v", wordCount, filePath))
-		return
+	if *byteCountPtr || !areFlagsSet {
+		fmt.Printf("%v\t", byteCount)
 	}
+
+	if *charCountPtr {
+		fmt.Printf("%v\t", charCount)
+	}
+
+	fmt.Printf("%v\n", filePath)
 }
 
-func countBySplit(input io.Reader, split bufio.SplitFunc) int {
+func count(input io.Reader) (int, int, int, int) {
 	scanner := bufio.NewScanner(input)
-	scanner.Split(split)
 
-	counter := 0
+	var lineCount, wordCount, charCount, byteCount int
 
 	for scanner.Scan() {
-		counter++
+		lineCount++
+		byteCount += len(scanner.Bytes()) + 2
+		charCount += len(scanner.Text()) + 2
+		wordCount += len(strings.Split(scanner.Text(), " "))
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error reading input:", err)
 	}
 
-	return counter
+	return lineCount, wordCount, charCount, byteCount
 }
